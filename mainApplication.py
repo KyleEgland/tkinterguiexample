c57b14e4d8cbe2e930233c2ec8ed21dbd21a3953
MediocreMarine <kyle.egland@outlook.com>
2018-07-29 14:09:36 -0500

    Added logging to main app

--- mainApplication.py
+++ mainApplication.py
@@ -4,11 +4,72 @@
 # Import information
 # tkinter = GUI module, used to construct GUI
 # ttk = tkinter styling module, more GUI stuff
+# logging = handles logging - see Logger Setup for more information
+# os = used here to handle directories - see Logger Setup
+# sys = used here to exit application before run - see Logger Setup
 import tkinter as tk
 import tkinter.ttk as ttk
+import logging
+import os
+import sys
+# "Local" imports - files that were created and should be placed in the same
+# directory as this file.
 from primarytab import PrimaryTab
 from alternatetab import AlternateTab
 from thirdtab import OtherTab
+
+
+# ------------ #
+# Logger Setup #
+# ------------ #
+# Check for the existence of a 'logs' folder - should one not exist, create it
+if os.path.exists('./logs/'):
+    pass
+else:
+    try:
+        os.mkdir('./logs/')
+    except Exception as e:
+        print('[-] Unable to create directory - please check permissions')
+        sys.exit()
+
+# Log levels (low-high): Debug -> Info -> Warning -> Error -> Critical
+# Instantiate a logger - instead of using root - to allow files to log
+# independently (if there are multiple files in a project)
+logger = logging.getLogger(__name__)
+
+# This establishes what level to log (ref. log levels above)
+logger.setLevel(logging.DEBUG)
+
+# Format the string that prepends the information that goes into the log file
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s',
+                              datefmt='%d-%b-%Y %H')
+
+# Create/name the log file
+file_handler = logging.FileHandler('./logs/{}.log'.format(__name__))
+
+# Link the specified format above to the logger
+file_handler.setFormatter(formatter)
+
+# Capture only Errors and above in file handler - this overrides ".setLevel"
+# for the file
+file_handler.setLevel(logging.ERROR)
+
+# Adding stream handler to put debug statements in console
+stream_handler = logging.StreamHandler()
+# Don't need to set logging level on this because the logger itself is
+# set to DEBUG already
+# Set formatting of stream handler to be the same as the log file
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+# Add stream handler to the logger
+logger.addHandler(stream_handler)
+# NOTES:
+# use logger.exception() to get the traceback in addtion to log message
+
+# ---------- #
+# End Logger #
+# ---------- #
 
 
 class MyApp(tk.Tk):
