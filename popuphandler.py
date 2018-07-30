31e6b844dabf1528ba1e680be6cb9be3948e76bd
MediocreMarine <kyle.egland@outlook.com>
2018-07-29 21:14:19 -0500

    Improvements and tests added to popuphandler.py

--- popuphandler.py
+++ popuphandler.py
@@ -69,18 +69,26 @@ class MessageBox(object):
         frm_2.pack(padx=4, pady=4)
 
         # Buttons
-        btn_1 = ttk.Button(frm_2, width=8, text=b1)
-        btn_1['command'] = self.b1_action
-        btn_1.pack(side='left')
+        if b2:
+            btn_1 = ttk.Button(frm_2, width=8, text=b1)
+            btn_1['command'] = self.b1_action
+            btn_1.pack(side='left')
+
+            btn_2 = ttk.Button(frm_2, width=8, text=b2)
+            btn_2['command'] = self.b2_action
+            btn_2.pack(side='left')
+        else:
+            btn_1 = ttk.Button(frm_2, width=8, text=b1)
+            btn_1['command'] = self.b1_action
+            btn_1.pack()
+
         if not entry:
             btn_1.focus_set()
-        btn_2 = ttk.Button(frm_2, width=8, text=b2)
-        btn_2['command'] = self.b2_action
-        btn_2.pack(side='left')
 
         # The enter button will trigger the focused button's action
         btn_1.bind('<KeyPress-Return>', func=self.b1_action)
-        btn_2.bind('<KeyPress-Return>', func=self.b2_action)
+        if b2:
+            btn_2.bind('<KeyPress-Return>', func=self.b2_action)
 
         # Roughly center the box on screen for accuracy see:
         # http://stackoverflow.com/a/10018670/1217270
@@ -102,27 +110,28 @@ class MessageBox(object):
     def b1_action(self, event=None):
         # First, check to see if there is an entry widget
         try:
-            x = self.entry.get()
+            self.returning = self.entry.get()
+            self.root.quit()
         except AttributeError:
             # If there is no entry widget, check for a dropbox widget
             try:
-                x = self.drpbx.get()
+                self.returning = self.drpbx.get()
+                self.root.quit()
             except AttributeError:
                 self.returning = self.b1_return
                 self.root.quit()
-            else:
-                if x:
-                    self.returning = x
-                    self.root.quit()
 
     def b2_action(self, event=None):
         self.returning = self.b2_return
         self.root.quit()
 
-    # remove this function and the call to protocol
-    # then the close button will act normally
+    # This function (using pass) prevents the "x" from closing the window.
+    # Should the window's "x" need to function normally, simply remove "pass"
+    # and input whatever code is appropriate.
     def close_mod(self):
         pass
+        # self.returning = False
+        # self.root.quit()
 
     def time_out(self):
         try:
@@ -166,26 +175,80 @@ def mbox(msg, b1='OK', b2='Cancel', frame=True, t=False, entry=False,
 
 # Tests - Run this file by itself to run the various tests below
 if __name__ == "__main__":
+
+    # define a function to be repeated at the end of every test that allows the
+    # user to exit testing before it has finished.
+    def pause():
+        try:
+            input("[*] Press ENTER to continue or Ctrl + c to exit.\n")
+        except KeyboardInterrupt:
+            print('\n[*] Tests interrupted, exiting')
+            sys.exit()
+
+    # --------------- #
+    # One button test #
+    # --------------- #
+    print('[*] Running test 1 - Ok only\n')
+    # This box should contain one button and return the value True
+    test1_box = mbox('Press "OK" to continue', b2=False, win_title='Test 1')
+    print(f'[*] Test 1 returned, {test1_box}\n')
+
+    pause()
+
     # -------------- #
     # OK/CANCEL test #
     # -------------- #
-    print('[*] Running test - ok, cancel\n')
+    print('[*] Running test 2 - ok, cancel\n')
     # This box should return either True (if ok is selected) or false (if
     # cancel is selected).
-    test1_box = mbox('Press "OK" or "Cancel"', win_title='Test 1')
-    print(f'[*] Pressed button returned:  {test1_box}\n')
-    try:
-        wait = input("[*] Press ENTER to continue or Ctrl + c to exit.\n")
-    except KeyboardInterrupt:
-        print('\n[*] Tests interrupted, exiting')
-        sys.exit()
+    test2_box = mbox('Press "OK" or "Cancel"', win_title='Test 2')
+    print(f'[*] Test 2 returned:  {test2_box}\n')
+
+    pause()
 
     # ----------------- #
     # Button Tuple Test #
     # ----------------- #
-    print('[*] Running test - Button Tuple\n')
+    print('[*] Running test 3 - Button Tuple\n')
     # This box will return either "OK" (if ok is selected) or "Cancel" (if
     # cancel is selected).
-    test2_box = mbox('Press "OK" or "Cancel"', b1=('OK', 'OK'),
-                     b2=('Cancel', 'Cancel'), win_title='Test 2')
-    print(f'[*] Pressed button returned:  {test2_box}\n')
+    test3_box = mbox('Press "OK" or "Cancel"', b1=('OK', 'OK'),
+                     b2=('Cancel', 'Cancel'), win_title='Test 3')
+    print(f'[*] Test 3 returned:  {test3_box}\n')
+
+    pause()
+
+    # ----------------- #
+    # Entry Widget Test #
+    # ----------------- #
+    print('[*] Running test 4 - Entry Widget\n')
+    # This window will incorporate an entry widget and return the contents of
+    # the entry widget.
+    test4_box = mbox('Enter something and press ok', entry=True,
+                     win_title='Test 4')
+    print(f'[*] Test 4 returned:  {test3_box}\n')
+
+    pause()
+
+    # ------------------- #
+    # Dropbox Widget Test #
+    # ------------------- #
+    print('[*] Running test 5 - Dropbox Widget\n')
+    # This window will incorporate a dropbox widget and return the selected
+    # option.
+    test5_box = mbox('Please make a selection', dropbox=True,
+                     elements=('Option1', 'Option2', 'Option3'),
+                     win_title='Test 5')
+    print(f'[*] Test 5 returned: {test5_box}\n')
+
+    pause()
+
+    # ---------- #
+    # Timer Test #
+    # ---------- #
+    print('[*] Running test 6 - Timeout test\n')
+    # This window will make use of the time out feature - wait 5 seconds and it
+    # will close itself
+    test6_box = mbox('Please wait 5 seconds for window to close', t=5,
+                     win_title='Test 6')
+    print(f'[*] Test 6 returned:  {test6_box}\n')
