a421cf895c568d2bb50967edadb63fc729a99701
MediocreMarine <kyle.egland@outlook.com>
2018-07-31 19:09:49 -0500

    Implemented entry widget add to txt box and scroll bar

--- alternatetab.py
+++ alternatetab.py
@@ -1,10 +1,12 @@
 #! python
 #
-# Alternate tab for Example GUI program
+# Alternate tab for Example GUI program.
+# This tab contains an entry and text box widget.
 # Import information:
 # tkinter = GUI module, used to construct the GUI
 # ttk = tkinter styling, more GUI stuff
 import tkinter as tk
+import tkinter.ttk as ttk
 # import tkinter.ttk as ttk
 
 
@@ -29,24 +31,40 @@ class AlternateTab(tk.Frame):
         self.entry_widget = tk.Entry(self)
         self.entry_widget.grid(row=0, column=1, padx=5, pady=5)
 
+        # bind the enter key to the entry widget
         self.key_bind()
 
         # Row 1
         # Text box
-        self.t_box = tk.Text(self)
+        self.t_box = tk.Text(self, width=40, height=12)
         # Insert some "default text" into the text box.  The '1.0' is a spacial
         # reference for where to insert text - 1 is line one and 0 is the 0th
         # character.  Line references start at 1 and character references start
         # at 0.
         self.t_box.insert('1.0', '(Change this text)')
         self.t_box.grid(row=1, columnspan=2, padx=5, pady=5, sticky='ew')
+        # Instantiate a scoll bar to interact with the text box - they are
+        # separate widgets themselves
+        self.txt_scroll = ttk.Scrollbar(self, orient='vertical',
+                                        command=self.t_box.yview)
+        # Place the scroll bar within the window
+        self.txt_scroll.grid(column=1, row=1, sticky='nse')
+        # Configure the text box so that the text box can interact with the
+        # scoll bar
+        self.t_box.configure(yscrollcommand=self.txt_scroll.set)
 
         # ----------------------- #
         # Alternate Tab Functions #
         # ----------------------- #
     def txt_box_update(self, event):
-        new_text = self.entry_widget.get()
+        # Get the contents of the entry widget and add a new line to the end of
+        # it.  This will push down the previous text and put the entry widget
+        # contents on top of it.
+        new_text = self.entry_widget.get() + "\n"
+        # Insert the newly created string into the text box
         self.t_box.insert('1.0', new_text)
+        # Delete what was in the entry widget
+        self.entry_widget.delete(0, 'end')
 
     # Bind the Enter key to the entry widget and the txt_box_update func
     def key_bind(self):
