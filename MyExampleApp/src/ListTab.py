4d525b4a3c3ffb93ded93b1bc5db1154611c1fa7
KyleEgland <kyle.egland@outlook.com>
2021-11-20 01:00:15 -0600

    completed list tab

--- MyExampleApp/src/ListTab.py
+++ MyExampleApp/src/ListTab.py
@@ -4,6 +4,7 @@
 # (ListTab) which provides an example of the listbox widget within tkinter.
 import logging
 import tkinter as tk
+from tkinter.constants import END
 import tkinter.ttk as ttk
 
 
@@ -33,6 +34,15 @@ class ListTab(tk.Frame):
         tk.Grid.columnconfigure(self, 2, weight=1)
         tk.Grid.columnconfigure(self, 3, weight=1)
 
+        # ------------- #
+        # Tab Variables #
+        # ------------- #
+        self.start_list = list()
+        self.start_list_variable = tk.StringVar(value=self.start_list)
+
+        self.other_list = list()
+        self.other_list_variable = tk.StringVar(value=self.other_list)
+
         # ------------------------ #
         # ROW 0 - Input Information#
         # ------------------------ #
@@ -45,6 +55,9 @@ class ListTab(tk.Frame):
         )
 
         self.input_info_entry = tk.Entry(self)
+        self.input_info_entry.bind(
+            "<Return>", lambda event=None: self.add_input_info()
+        )
         self.input_info_entry.grid(
             row=0, column=1, sticky='ew', columnspan=2
         )
@@ -59,7 +72,10 @@ class ListTab(tk.Frame):
         # ----------------------------------------_------ #
         # ROW 1 - top of list boxes and move_right_button #
         # -----------------------------------------_----- #
-        self.start_listbox = tk.Listbox(self)
+        self.start_listbox = tk.Listbox(
+            self, listvariable=self.start_list_variable
+        )
+
         self.start_listbox.grid(
             row=1, column=1, rowspan=3, sticky="nsew", padx=15, pady=15
         )
@@ -71,7 +87,9 @@ class ListTab(tk.Frame):
             row=1, column=2
         )
 
-        self.other_listbox = tk.Listbox(self)
+        self.other_listbox = tk.Listbox(
+            self, listvariable=self.other_list_variable
+        )
         self.other_listbox.grid(
             row=1, column=3, rowspan=3, sticky="nsew", padx=15, pady=15
         )
@@ -112,14 +130,52 @@ class ListTab(tk.Frame):
     def add_input_info(self):
         # add_input_info retrieves the information from the entry widget and
         # "adds it" to the start_listbox widget
-        pass
+        addition = self.input_info_entry.get()
+
+        if addition == "":
+            self.logger.info("Nothing added to start list")
+            return
+
+        # Remove the information from the text box widget
+        self.input_info_entry.delete(0, END)
+
+        # Add the new text to the start_list
+        self.start_list.append(addition)
+        # Update the list variable
+        self.start_list_variable.set(self.start_list)
+
+        self.logger.info(f"New entry to start_list: {addition}")
 
     def move_item_right(self):
         # move_item_right moves an item from the "start_listbox" to the
         # "other_listbox"
-        pass
+        try:
+            # moving is a list of indices, we just want the first
+            moving = self.start_listbox.curselection()[0]
+        except IndexError:
+            self.logger.warning("No start_listbox item selected.")
+            return
+        moving_string = self.start_list[moving]
+
+        self.start_list.remove(moving_string)
+        self.start_list_variable.set(self.start_list)
+
+        self.other_list.append(moving_string)
+        self.other_list_variable.set(self.other_list)
 
     def move_item_left(self):
         # move_item_left moves an item from the "other_listbox" to the
         # "start_listbox"
-        pass
+        try:
+            moving = self.other_listbox.curselection()[0]
+        except IndexError:
+            self.logger.warning("No other_listbox item selected.")
+            return
+
+        moving_string = self.other_list[moving]
+
+        self.other_list.remove(moving_string)
+        self.other_list_variable.set(self.start_list)
+
+        self.start_list.append(moving_string)
+        self.start_list_variable.set(self.start_list)
